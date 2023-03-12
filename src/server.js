bf138e4dd7cb47a9a53f34e43ad7f8d0cd1ed9e8
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-03-12 11:41:20 +0530

    server up

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express,{json} from "express";
+import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -12,20 +12,27 @@ import s5002Router from "./routes/s5002routes/s5002router.js";
 //import validateAndLogin from "./routes/AuthRoutes/login/validateAndLogin.js";
 //import verifyGmail from "./routes/AuthRoutes/verifyGmail/verifyGmail.js";
 
-
-
-
 const app = express();
 
+// const corsOptions = {
+//   origin: process.env.ALLOWED_ORIGIN,
+//   methods: ["POST"],
+// };
+//sent with response.headers
 app.use(cors());
+
+app.use((req, res, next) => {
+  if (req.headers.origin === process.env.ALLOWED_ORIGIN) next();
+  else res.status(500).send("unexpected_error");
+});
+
 app.use(json());
 
-app.use("/auth",authRouter);
+app.use("/auth", authRouter);
 
-app.use("/s5002",s5002Router);
-
+app.use("/s5002", s5002Router);
 
 app.listen(5001, () => {
-    console.log("listening to port 5001");
-    //clearExpiredVerifications(300000);
+  console.log("listening to port 5001");
+  //clearExpiredVerifications(300000);
 });
