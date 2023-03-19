bb2ba515b5cbf8759026e9daedfab3e688780249
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-03-19 09:24:56 +0530

    up

--- src/controllers/maindbPool.js
+++ src/controllers/maindbPool.js
@@ -5,11 +5,12 @@ dotenv.config();
 //this code runs before original initialization
 
 const poolData = {
-  connectionLimit: 10,
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_MAIN,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 100,
+  waitForConnections: true
 };
 
 const pool = mysql.createPool(poolData);
